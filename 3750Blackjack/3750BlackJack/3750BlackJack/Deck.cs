e488c6928f106fea93b51c0ce6a971eb77cbde1c
RXtr <RobbieXtreme@gmail.com>
2016-08-31 18:13:29 -0600

    Most of the logic for Deck.cs; Will need to interface with Card.cs when it's implemented

--- 3750Blackjack/3750BlackJack/3750BlackJack/Deck.cs
+++ 3750Blackjack/3750BlackJack/3750BlackJack/Deck.cs
@@ -8,15 +8,86 @@ namespace _3750BlackJack
 {
     class Deck
     {
-        public List<Card> Cards { get; set; }
+        #region Properties
+        
+        /// <summary>
+        /// Represents a deck of cards
+        /// </summary>
+        private List<Card> Cards { get; set; }
+
+        #endregion //Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Draws a card from the top of the deck.
+        /// </summary>
+        /// <returns></returns>
         public Card Draw()
         {
-            return new Card();
+            //remove card on top and return it.
+            Card cardOnTop = Cards[0];
+            Cards.Remove(Cards[0]);
+            return cardOnTop;
         }
 
+        /// <summary>
+        /// Burns old deck. Builds new one. Shuffle
+        /// </summary>
         public void Shuffle()
         {
+            //burn the old deck
+            Cards = new List<Card>();
+            
+            //build new one
+            BuildDeck();
 
+            //shuffle
+            ActuallyShuffle(Cards);
         }
+
+        /// <summary>
+        /// Adds new cards to the deck
+        /// </summary>
+        private void BuildDeck()
+        {
+            //for each Suit
+            for(int i = 0; i < 4; i++)
+            {
+                //for each Value
+                for(int j = 0; j < 14; j++)
+                {
+                    //add a the next new card
+                    //TODO: wait until the Constructor for Card is finished to add property arguments. 
+                    Cards.Add(new Card());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shuffles a list of cards.
+        /// </summary>
+        /// <typeparam name="Card"></typeparam>
+        /// <param name="deck">a deck of cards</param>
+        private void ActuallyShuffle<Card>(List<Card> deck)
+        {
+            //get deck count
+            int n = deck.Count;
+            Random r = new Random();
+
+            //for each card in the deck
+            while (n > 1)
+            {
+                //find random spot
+                int k = r.Next(0, n);
+                n--;
+                //swap with other card
+                Card temp = deck[k];
+                deck[k] = deck[n];
+                deck[n] = temp;
+            }
+        }
+
+        #endregion //Methods
     }
 }
